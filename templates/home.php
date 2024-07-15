0b743bcfeda9f518b8725b8a76f70cd0f9b3c9bb
varsha-chudasama <147918626+varsha-chudasama@users.noreply.github.com>
2024-07-15 13:08:52 +0530

    home page

--- templates/home.php
+++ templates/home.php
@@ -31,13 +31,17 @@
   <div class="spacing dmb-60"></div>
 
   <section class="center-content-section">
-    <div class="container">
-      <div class="col-7 mx-auto text-center">
-        <div class="bodoni font40 leading48 text-191919 dmb-25">The ultimate homemaker.</div>
-        <div class="montserrat-regular font13 leading23 text-191919 col-11 mx-auto">Over 20 years in the business has given us great   insight into what our customers want and need. With customer satisfaction as our top priority, we source furniture, cutting out the middle man and therefore reducing the price for our clients.</div>
-      </div>
-    </div>
-  </section>
+        <div class="container">
+            <div class="col-7 px-5 mx-auto text-center">
+                <div class="content-title bodoni font40 leading48 text-191919 dmb-25">The ultimate homemaker.</div>
+                <div class="content-desc montserrat-regular font13 leading23 text-191919">Over 20 years
+                    in the
+                    business has given us great insight into what our customers want and need. With customer
+                    satisfaction as our top priority, we source furniture, cutting out the middle man and therefore
+                    reducing the price for our clients.</div>
+            </div>
+        </div>
+    </section>
 
   <div class="spacing dmb-60"></div>
 
@@ -102,12 +106,142 @@
     <div class="container">
       <div class="bodoni font40 leading48 text-black text-center dmb-60">Trending products</div>
       <div class="product-slider">
-        <a href="">
-          <div>
-            <img src="<?php echo get_img_url() ?>/wp-content/uploads/2024/07/treding-img.png" class="w-100 h-100 object-cover" alt="">
+        <a href="" class="d-inline-block text-decoration-none card-hover">
+          <div class="product-img position-relative radius10 overflow-hidden dmb-25">
+            <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/treding-img.png" class="w-100 h-100 object-cover img" alt="">
+            <div class="montserrat-medium font13 leading20 text-292929 bg-white radius5 position-absolute top-0 start-0 py-1 px-2 mt-3 ms-4">New in</div>
+            <div class="like-img position-absolute bottom-0 end-0 me-3 mb-2">
+              <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/like.svg" class="w-100" alt="">
+            </div>
+          </div>
+          <div class="product-content">
+            <div class="montserrat-medium font16 leading20 text-black dpb-10">Gardena Sheepskin Armchair</div>
+            <div class="montserrat-medium font13 leading20 text-7D7D7D">£995.00</div>
+          </div>
+        </a>
+        <a href="" class="d-inline-block text-decoration-none card-hover">
+          <div class="product-img position-relative radius10 overflow-hidden dmb-25">
+            <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/treding-img.png" class="w-100 h-100 object-cover img" alt="">
+            <div class="montserrat-medium font13 leading20 text-292929 bg-white radius5 position-absolute top-0 start-0 py-1 px-2 mt-3 ms-4">New in</div>
+            <div class="like-img position-absolute bottom-0 end-0 me-3 mb-2">
+              <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/like.svg" class="w-100" alt="">
+            </div>
+          </div>
+          <div class="product-content">
+            <div class="montserrat-medium font16 leading20 text-black dpb-10">Gardena Sheepskin Armchair</div>
+            <div class="montserrat-medium font13 leading20 text-7D7D7D">£995.00</div>
+          </div>
+        </a>
+        <a href="" class="d-inline-block text-decoration-none card-hover">
+          <div class="product-img position-relative radius10 overflow-hidden dmb-25">
+            <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/treding-img.png" class="w-100 h-100 object-cover img" alt="">
+            <div class="montserrat-medium font13 leading20 text-292929 bg-white radius5 position-absolute top-0 start-0 py-1 px-2 mt-3 ms-4">New in</div>
+            <div class="like-img position-absolute bottom-0 end-0 me-3 mb-2">
+              <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/like.svg" class="w-100" alt="">
+            </div>
+          </div>
+          <div class="product-content">
+            <div class="montserrat-medium font16 leading20 text-black dpb-10">Gardena Sheepskin Armchair</div>
+            <div class="montserrat-medium font13 leading20 text-7D7D7D">£995.00</div>
+          </div>
+        </a>
+        <a href="" class="d-inline-block text-decoration-none card-hover">
+          <div class="product-img position-relative radius10 overflow-hidden dmb-25">
+            <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/treding-img.png" class="w-100 h-100 object-cover img" alt="">
+            <div class="montserrat-medium font13 leading20 text-292929 bg-white radius5 position-absolute top-0 start-0 py-1 px-2 mt-3 ms-4">New in</div>
+            <div class="like-img position-absolute bottom-0 end-0 me-3 mb-2">
+              <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/like.svg" class="w-100" alt="">
+            </div>
+          </div>
+          <div class="product-content">
+            <div class="montserrat-medium font16 leading20 text-black dpb-10">Gardena Sheepskin Armchair</div>
+            <div class="montserrat-medium font13 leading20 text-7D7D7D">£995.00</div>
+          </div>
+        </a>
+        <a href="" class="d-inline-block text-decoration-none card-hover">
+          <div class="product-img position-relative radius10 overflow-hidden dmb-25">
+            <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/treding-img.png" class="w-100 h-100 object-cover img" alt="">
+            <div class="montserrat-medium font13 leading20 text-292929 bg-white radius5 position-absolute top-0 start-0 py-1 px-2 mt-3 ms-4">New in</div>
+            <div class="like-img position-absolute bottom-0 end-0 me-3 mb-2">
+              <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/like.svg" class="w-100" alt="">
+            </div>
+          </div>
+          <div class="product-content">
+            <div class="montserrat-medium font16 leading20 text-black dpb-10">Gardena Sheepskin Armchair</div>
+            <div class="montserrat-medium font13 leading20 text-7D7D7D">£995.00</div>
           </div>
         </a>
       </div>
     </div>
   </section>
+
+  <div class="spacing dmb-80"></div>
+
+  <section class="room-card-section bg-292929 dpt-80 dpb-70">
+      <div class="container">
+        <div class="bodoni font45 leading54 text-capitalize text-white text-center dpb-50">Shop By Room</div>
+        <div class="room-card-slider">
+            <a href="" class="d-inline-block text-decoration-none card-hover">
+              <div class="room-card-img overflow-hidden radius10 position-relative">
+                <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/our-img2.png" class="h-100 w-100 object-cover img" alt="">
+                <div class="position-absolute top-0 start-0 h-100 w-100 bg-black opacity23"></div>
+                <div class="position-absolute bottom-0 ps-5">
+                  <div class="bodoni font40 leading48 text-white dpb-20">Bedroom</div>
+                  <div class="montserrat-medium font13 leading20 text-white text-capitalize text-decoration-underline dpb-55">Shop Collection</div>
+                </div>
+              </div>
+            </a>
+            <a href="" class="d-inline-block text-decoration-none card-hover">
+              <div class="room-card-img overflow-hidden radius10 position-relative">
+                <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/our-img2.png" class="h-100 w-100 object-cover img" alt="">
+                <div class="position-absolute top-0 start-0 h-100 w-100 bg-black opacity23"></div>
+                <div class="position-absolute bottom-0 ps-5">
+                  <div class="bodoni font40 leading48 text-white dpb-20">Bedroom</div>
+                  <div class="montserrat-medium font13 leading20 text-white text-capitalize text-decoration-underline dpb-55">Shop Collection</div>
+                </div>
+              </div>
+            </a>
+            <a href="" class="d-inline-block text-decoration-none card-hover">
+              <div class="room-card-img overflow-hidden radius10 position-relative">
+                <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/our-img2.png" class="h-100 w-100 object-cover img" alt="">
+                <div class="position-absolute top-0 start-0 h-100 w-100 bg-black opacity23"></div>
+                <div class="position-absolute bottom-0 ps-5">
+                  <div class="bodoni font40 leading48 text-white dpb-20">Bedroom</div>
+                  <div class="montserrat-medium font13 leading20 text-white text-capitalize text-decoration-underline dpb-55">Shop Collection</div>
+                </div>
+              </div>
+            </a>
+            <a href="" class="d-inline-block text-decoration-none card-hover">
+              <div class="room-card-img overflow-hidden radius10 position-relative">
+                <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/our-img2.png" class="h-100 w-100 object-cover img" alt="">
+                <div class="position-absolute top-0 start-0 h-100 w-100 bg-black opacity23"></div>
+                <div class="position-absolute bottom-0 ps-5">
+                  <div class="bodoni font40 leading48 text-white dpb-20">Bedroom</div>
+                  <div class="montserrat-medium font13 leading20 text-white text-capitalize text-decoration-underline dpb-55">Shop Collection</div>
+                </div>
+              </div>
+            </a>
+        </div>
+      </div>
+  </section>
+
+  <section class="banner-section position-relative">
+      <div class="banner-img">
+        <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/hero-img.png" alt="" class="h-100 w-100 object-cover">
+      </div>
+      <div class="position-absolute top-center start-0 w-100">
+       <div class="container w-100">
+       <div class="bodoni font60 leading72 text-capitalize text-white dmb-10">Our Showroom</div>
+       <div class="col-8">
+       <div class="montserrat-medium font14 leading18 text-white dmb-35">The UK’s finest selection of domestic, bar and restaurant furniture</div>
+        <div class="d-flex align-items-center">
+        <a href="" class="text-decoration-none btnA white-border-btn d-inline-block montserrat-semibold font11 leading20 space0_28 text-uppercase radius5 d-flex align-items-center justify-content-center transition">
+        LEARN MORE
+        </a>
+      </div>
+       </div>
+       </div>
+      </div>
+  </section>
+
 </main>
