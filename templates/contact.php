bac271030480897e1ac335b8dc2bd235938d1126
jigishachavda <jigishachavda1011@gmail.com>
2024-07-16 09:52:50 +0530

    contact page hero section

--- templates/contact.php
+++ templates/contact.php
@@ -1,5 +1,139 @@
 <?php
 /*
-  * Template Name: contact
-*/
+ * Template Name: contact
+ */
 ?>
+<!-- inspiration-hero-section -->
+<section class="inspiration-hero-section position-relative">
+    <div class="inspiration-hero-img h-100 position-relative">
+        <img src="<?php echo get_home_url(); ?>/wp-content/uploads/2024/07/hero-img.png"
+            class="w-100 h-100 object-cover" alt="">
+        <div class="layer position-absolute top-0 start-0 bg-black opacity29 h-100 w-100"></div>
+    </div>
+    <div class="position-absolute bottom-0 start-0 w-100 inspiration-hero-content">
+        <div class="container w-100 h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-8">
+                    <div class="bodoni font65 leading85 text-white dmb-10">Visit Our Showroom</div>
+                    <div class="mont-medium font14 leading20 text-white dmb-20">
+                        The UK’s finest selection of domestic, bar and restaurant furniture
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <a href=""
+                            class="text-decoration-none btnA white-border-btn mont-semibold font11 leading20 space0_28 text-uppercase radius5 d-flex align-items-center justify-content-center transition">
+                            GET DIRECTIONS
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="spacing dmb-90"></div>
+
+<!-- center-content-section  -->
+<section class="center-content-section">
+    <div class="container">
+        <div class="col-7 px-5 mx-auto text-center">
+            <div class="content-title bodoni font11 leading20 text-191919 dmb-35">Contact</div>
+            <div class="content-title bodoni font40 leading48 text-191919 dmb-35">We’d love to help you!</div>
+            <div class="content-desc mont-normal font13 leading24 text-191919">
+                We’d love to help you know more about our furniture collection… Why not give our team a call or fill out
+                the contact form below. If you have an enquiry, order or a customised product you would like to discuss
+                please give us as much detail as possible and we will get back to you as soon as we can. If you have an
+                item you wish to sell to us please email us at purchasing@peppermillinteriors.com with a photo of the
+                item/s you are selling, the price you are looking to sell each item for and the location of where you
+                are based.
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="spacing dmb-105"></div>
+
+<!-- form-section -->
+<section class="contact-section">
+    <div class="container">
+        <div class="col-6 px-2 mx-auto">
+            <div class="text-center dmb-50">
+                <div class="bodoni font35 leading45 text-black dmb-15">
+                    Fill out our contact form below
+                </div>
+                <div class="content-desc mont-normal font13 leading24 text-191919">
+                    Or call us on 01543 375 872
+                </div>
+            </div>
+            <form action="" class="form-section d-flex flex-wrap">
+                <div class="col-6 dmb-10">
+                    <input type="text" placeholder="Your Name…"
+                        class="input-field w-100 mont-medium font12 leading16 text-918E8E radius5 px-4">
+                </div>
+                <div class="col-6 dmb-10">
+                    <input type="email" placeholder="Email…"
+                        class="input-field w-100 mont-medium font12 leading16 text-918E8E radius5 px-4">
+                </div>
+                <div class="col-6 dmb-10">
+                    <input type="number" placeholder="Phone Number…"
+                        class="input-field w-100 mont-medium font12 leading16 text-918E8E radius5 px-4">
+                </div>
+                <div class="col-6 dmb-10">
+                    <input type="text" placeholder="Company Name (If Applicable)…"
+                        class="input-field w-100 mont-medium font12 leading16 text-918E8E radius5 px-4">
+                </div>
+                <div class="col-12 dmb-10">
+                    <div class="mont-medium font12 leading16 text-black dmb-15 dmt-10">Are you looking for products for
+                        your
+                        home or business?</div>
+                    <input type="text" placeholder="Please Select…"
+                        class="input-field w-100 mont-medium font12 leading16 text-918E8E radius5 px-4">
+                </div>
+                <div class="col-12 dmb-10">
+                    <div class="mont-medium font12 leading16 text-black dmb-15 dmt-10">Are you looking for products for
+                        your
+                        home or business?</div>
+                    <input type="text" placeholder="Start Typing…"
+                        class="input-field w-100 mont-medium font12 leading16 text-918E8E radius5 px-4">
+                </div>
+                <div class="col-12 dmb-10">
+                    <div class="mont-medium font12 leading16 text-black dmb-15 dmt-10">Are you looking for products for
+                        your
+                        home or business?</div>
+                    <input type="text" placeholder="Please select…"
+                        class="input-field w-100 mont-medium font12 leading16 text-918E8E radius5 px-4">
+                </div>
+            </form>
+            <div class="dmt-45 d-flex align-items-center justify-content-center">
+                <a href=""
+                    class="text-decoration-none btnA bg-292929-btn mont-semibold font11 leading20 space0_28 text-uppercase radius5 transition d-inline-flex align-items-center justify-content-center">
+                    SUBMIT ENQUIRY
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="spacing dmb-165"></div>
+
+<section class="location-card-section">
+    <div class="container">
+        <div class="location-card">
+            <div class="location-img">
+                <img src="" alt="">
+            </div>
+            <div class="location-content">
+                <div>Burntwood (Showroom)</div>
+                <a href="text-decoration-none d-inline-block ">
+                    Peppermill Antiques Ltd, Unit 6, Ring Road, Zone 2, Burntwood Business Park Burntwood, WS7 3JQ, (Sat
+                    Nav use WS7 3JG)
+                </a>
+                <a href="">
+                    What3words: work.trendy.exists
+                </a>
+                <a href="">
+                    Tel: 01543 375 872 Int: 0044 1543 375 872
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
