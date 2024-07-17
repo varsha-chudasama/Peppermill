35bd452f81dc3cd364c15ec4bf3c10de3f8fa5c5
jigishachavda <jigishachavda1011@gmail.com>
2024-07-17 11:53:01 +0530

    faq section

---
+++ templates/faq.php
@@ -0,0 +1,141 @@
+<?php
+/*
+ * Template Name: faq
+ */
+?>
+
+<main>
+    <div class="spacing dmt-200"></div>
+
+    <!-- faq-accordion -->
+    <section class="faq-section">
+        <div class="container">
+            <div class="col-5 px-3 mx-auto dmb-70">
+                <div class="bodoni font40 leading48 text-center dmb-30">
+                    Frequently Asked Questions
+                </div>
+                <div class="mont-medium font13 leading24 text-center">
+                    Frequently asked questions are listed below. If you can’t find the answer here, don’t hesitate
+                    to contact us.
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-8 px-1 mx-auto">
+                    <div class="closet-accordion">
+                        <div class="closet-cards cursor-pointer border-bottom border-EBEBEB">
+                            <div class="closet-title d-flex align-items-center justify-content-between active">
+                                <div class="mont-medium font16 leading20 text-black">
+                                    Do you ship internationally?
+                                </div>
+                                <div class="closet-closebtn d-flex-align-items-center">
+                                    <img src="<?php echo get_home_url(); ?>" alt="" class="w-100">
+                                </div>
+                            </div>
+                            <div class="closet-content dmt-20 dmb-10 pe-3">
+                                <div class="col-11 pe-4 mont-normal font13 leading24 text-black">
+                                    We are based in Burntwood, Staffordshire (around a 40 minute drive from
+                                    Birmingham),
+                                    and if you fancy a browse our showroom is open to the public Monday to Saturday.
+                                    If
+                                    you can’t make it into store, don’t worry, you can place your order online or
+                                    over
+                                    the phone and we ship all over the world.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="closet-cards cursor-pointer border-bottom border-EBEBEB">
+                            <div class="closet-title d-flex align-items-center justify-content-between active">
+                                <div class="mont-medium font16 leading20 text-black">
+                                    Do you ship internationally?
+                                </div>
+                                <div class="closet-closebtn d-flex-align-items-center">
+                                    <img src="<?php echo get_home_url(); ?>" alt="" class="w-100">
+                                </div>
+                            </div>
+                            <div class="closet-content dmt-20 dmb-10 pe-3">
+                                <div class="col-11 pe-4 mont-normal font13 leading24 text-black">
+                                    We are based in Burntwood, Staffordshire (around a 40 minute drive from
+                                    Birmingham),
+                                    and if you fancy a browse our showroom is open to the public Monday to Saturday.
+                                    If
+                                    you can’t make it into store, don’t worry, you can place your order online or
+                                    over
+                                    the phone and we ship all over the world.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="closet-cards cursor-pointer border-bottom border-EBEBEB">
+                            <div class="closet-title d-flex align-items-center justify-content-between active">
+                                <div class="mont-medium font16 leading20 text-black">
+                                    Do you ship internationally?
+                                </div>
+                                <div class="closet-closebtn d-flex-align-items-center">
+                                    <img src="<?php echo get_home_url(); ?>" alt="" class="w-100">
+                                </div>
+                            </div>
+                            <div class="closet-content dmt-20 dmb-10 pe-3">
+                                <div class="col-11 pe-4 mont-normal font13 leading24 text-black">
+                                    We are based in Burntwood, Staffordshire (around a 40 minute drive from
+                                    Birmingham),
+                                    and if you fancy a browse our showroom is open to the public Monday to Saturday.
+                                    If
+                                    you can’t make it into store, don’t worry, you can place your order online or
+                                    over
+                                    the phone and we ship all over the world.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="closet-cards cursor-pointer border-bottom border-EBEBEB">
+                            <div class="closet-title d-flex align-items-center justify-content-between active">
+                                <div class="mont-medium font16 leading20 text-black">
+                                    Do you ship internationally?
+                                </div>
+                                <div class="closet-closebtn d-flex-align-items-center">
+                                    <img src="<?php echo get_home_url(); ?>" alt="" class="w-100">
+                                </div>
+                            </div>
+                            <div class="closet-content dmt-20 dmb-10 pe-3">
+                                <div class="col-11 pe-4 mont-normal font13 leading24 text-black">
+                                    We are based in Burntwood, Staffordshire (around a 40 minute drive from
+                                    Birmingham),
+                                    and if you fancy a browse our showroom is open to the public Monday to Saturday.
+                                    If
+                                    you can’t make it into store, don’t worry, you can place your order online or
+                                    over
+                                    the phone and we ship all over the world.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="spacing dmt-120"></div>
+
+    <!-- learn-more-section -->
+    <section class="learn-more-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 mx-auto">
+                    <div class="bodoni font40 leading48 text-black text-center dmb-25">
+                        Can’t find what you’re looking for?
+                    </div>
+                    <div class="mont-normal font13 leading24 text-black text-center dmb-30">
+                        Contact us now to find out how we can help you achieve your dream commercial space
+                    </div>
+                    <div class="w-100 d-inline-flex justify-content-center ">
+                        <a href=""
+                            class="text-decoration-none btnA bg-292929-btn mont-semibold font11 leading20 space0_28 text-uppercase radius5 transition d-inline-flex align-items-center justify-content-center">
+                            GET IN TOUCH
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="spacing dmt-105"></div>
+
+</main>
