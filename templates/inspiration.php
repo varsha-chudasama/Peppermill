051473ac12b408847a960a1a8eaf19d14dda4025
truptikarmadhi <132331501+truptikarmadhi@users.noreply.github.com>
2024-07-18 12:36:24 +0530

    inspiration page complete

--- templates/inspiration.php
+++ templates/inspiration.php
@@ -1,30 +1,34 @@
 <?php
 /*
- * Template Name: Home
+ * Template Name: inspiration
  */
 ?>
 
-
 <main>
-    <!-- hero-section -->
-    <section class="hero-section position-relative w-100">
-        <div class="home-hero-img h-100 position-relative">
+    <!-- inspiration-hero-section -->
+    <section class="inspiration-hero-section position-relative">
+        <div class="inspiration-hero-img h-100 position-relative">
             <img src="<?php echo get_home_url(); ?>/wp-content/uploads/2024/07/home-hero.png"
                 class="w-100 h-100 object-cover" alt="">
             <div class="layer position-absolute top-0 start-0 bg-black opacity29 h-100 w-100"></div>
         </div>
-        <div class="position-absolute top-center start-0 w-100 text-center hero-content">
-            <div class="container w-100">
-                <div class="col-8 mx-auto">
-                    <div class="mont-semibold font11 leading20 space1_65 text-uppercase text-white dmb-30">WELCOME TO
-                        PEPPERMILL</div>
-                    <div class="bodoni font75 leading90 text-white dmb-35">Spice up the ordinary.</div>
-                    <div class="mont-medium font14 leading20 text-white dmb-25">The UK’s finest selection of domestic,
-                        bar and restaurant furniture</div>
-                    <a href=""
-                        class="text-decoration-none btnA white-border-btn mont-semibold font11 leading20 space0_28 text-uppercase radius5 d-inline-flex align-items-center justify-content-center transition">
-                        SHOP NOW
-                    </a>
+        <div class="position-absolute bottom-0 start-0 w-100 inspiration-hero-content">
+            <div class="container w-100 h-100">
+                <div class="row h-100 align-items-center">
+                    <div class="col-8 ms-5 ps-5">
+                        <div class="mont-semibold font11 leading20 space1_65 text-white text-uppercase dmb-45">
+                            INSPIRATION</div>
+                        <div class="bodoni font65 leading85 text-white dmb-10">The ultimate homemaker.</div>
+                        <div class="mont-medium font14 leading20 text-white dmb-30">
+                            The UK’s finest selection of domestic, bar and restaurant furniture
+                        </div>
+                        <div class="d-flex align-items-center">
+                            <a href=""
+                                class="text-decoration-none btnA white-border-btn mont-semibold font11 leading20 space0_28 text-uppercase radius5 d-flex align-items-center justify-content-center transition">
+                                SHOP NOW
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -32,68 +36,106 @@
 
     <div class="spacing dmb-60"></div>
 
-    <!-- center-content-section -->
-    <section class="center-content-section">
+    <section class="collection-card-section">
         <div class="container">
-            <div class="col-7 px-5 mx-auto text-center">
-                <div class="content-title bodoni font40 leading48 dmb-25">The ultimate homemaker.</div>
-                <div class="content-desc mont-normal font13 leading24">Over 20 years
-                    in the
-                    business has given us great insight into what our customers want and need. With customer
-                    satisfaction as our top priority, we source furniture, cutting out the middle man and therefore
-                    reducing the price for our clients.</div>
+            <div class="col-7 mx-auto dmb-50">
+                <div class="bodoni font40 leading45 text-center text-black dmb-25">
+                    Explore collections
+                </div>
+                <div class="mont-medium font13 leading24 text-center text-7D7D7D">
+                    Explore our buying guide to find the right design and style for furnishing your home.
+                </div>
             </div>
-        </div>
-    </section>
-
-    <div class="spacing dmb-60"></div>
-
-    <!-- three-card-section -->
-    <section class="three-card-section">
-        <div class="container">
             <div class="row row10">
-                <div class="col-4 cards-col dmt-20">
-                    <a href=""
-                        class="the-ultimate-card d-inline-block text-decoration-none card-hover radius10 overflow-hidden position-relative w-100">
-                        <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/three-card-1.png"
-                            class="w-100 h-100 object-cover img" alt="">
-                        <div class="layer position-absolute top-0 start-0 h-100 w-100 opacity23 bg-black"></div>
-                        <div class="position-absolute top-center text-center w-100 px-5">
-                            <div class="bodoni font35 leading35 text-capitalize text-white dmb-30">New In</div>
-                            <button
-                                class="text-decoration-none btnA white-border-btn mont-semibold font11 leading20 space0_28 text-uppercase radius5 d-inline-flex align-items-center justify-content-center transition">
-                                SHOP NOW
-                            </button>
+                <div class="col-6 dmb-20 cards-col">
+                    <a href="" class="w-100 text-decoration-none">
+                        <div class="collection-cards w-100 radius10 position-relative overflow-hidden  card-hover">
+                            <img src="<?php echo get_home_url(); ?>/wp-content/uploads/2024/07/inspiration-1.png" alt=""
+                                class="w-100 h-100 object-cover">
+                            <div class="position-absolute w-100 h-100 top-0 bg-black opacity-25"></div>
+                            <div
+                                class="position-absolute w-100 h-100 top-0 d-flex flex-column justify-content-end px-5 dpb-60">
+                                <div class="bodoni font40 leading45 text-white dmb-10">
+                                    Seating
+                                </div>
+                                <div class="mont-medium font14 leading24 text-white dmb-15">
+                                    The UK’s finest selection of domestic, bar and restaurant furniture
+                                </div>
+                                <div class="d-inline-flex">
+                                    <div class="mont-medium font13 leading24 text-white text-capitalize border-bottom">
+                                        Shop
+                                        Collection</div>
+                                </div>
+                            </div>
                         </div>
                     </a>
                 </div>
-                <div class="col-4 cards-col dmt-20">
-                    <a href=""
-                        class="the-ultimate-card d-inline-block text-decoration-none card-hover radius10 overflow-hidden position-relative w-100">
-                        <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/three-card-2.png"
-                            class="w-100 h-100 object-cover img" alt="">
-                        <div class="layer position-absolute top-0 start-0 h-100 w-100 opacity23 bg-black"></div>
-                        <div class="position-absolute top-center text-center w-100 px-5">
-                            <div class="bodoni font35 leading35 text-capitalize text-white dmb-30">New In</div>
-                            <button
-                                class="text-decoration-none btnA white-border-btn mont-semibold font11 leading20 space0_28 text-uppercase radius5 d-inline-flex align-items-center justify-content-center transition">
-                                SHOP NOW
-                            </button>
+                <div class="col-6 dmb-20 cards-col">
+                    <a href="" class="w-100 text-decoration-none">
+                        <div class="collection-cards w-100 radius10 position-relative overflow-hidden  card-hover">
+                            <img src="<?php echo get_home_url(); ?>/wp-content/uploads/2024/07/inspiration-2.png" alt=""
+                                class="w-100 h-100 object-cover">
+                            <div class="position-absolute w-100 h-100 top-0 bg-black opacity-25"></div>
+                            <div
+                                class="position-absolute w-100 h-100 top-0 d-flex flex-column justify-content-end px-5 dpb-60">
+                                <div class="bodoni font40 leading45 text-white dmb-10">
+                                    Tables
+                                </div>
+                                <div class="mont-medium font14 leading24 text-white dmb-15">
+                                    The UK’s finest selection of domestic, bar and restaurant furniture
+                                </div>
+                                <div class="d-inline-flex">
+                                    <div class="mont-medium font13 leading24 text-white text-capitalize border-bottom">
+                                        Shop
+                                        Collection</div>
+                                </div>
+                            </div>
                         </div>
                     </a>
                 </div>
-                <div class="col-4 cards-col dmt-20">
-                    <a href=""
-                        class="the-ultimate-card d-inline-block text-decoration-none card-hover radius10 overflow-hidden position-relative w-100">
-                        <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/three-card-3.png"
-                            class="w-100 h-100 object-cover img" alt="">
-                        <div class="layer position-absolute top-0 start-0 h-100 w-100 opacity23 bg-black"></div>
-                        <div class="position-absolute top-center text-center w-100 px-5">
-                            <div class="bodoni font35 leading35 text-capitalize text-white dmb-30">New In</div>
-                            <button
-                                class="text-decoration-none btnA white-border-btn mont-semibold font11 leading20 space0_28 text-uppercase radius5 d-inline-flex align-items-center justify-content-center transition">
-                                SHOP NOW
-                            </button>
+                <div class="col-6 dmb-20 cards-col">
+                    <a href="" class="w-100 text-decoration-none">
+                        <div class="collection-cards w-100 radius10 position-relative overflow-hidden  card-hover">
+                            <img src="<?php echo get_home_url(); ?>/wp-content/uploads/2024/07/home-hero.png" alt=""
+                                class="w-100 h-100 object-cover">
+                            <div class="position-absolute w-100 h-100 top-0 bg-black opacity-25"></div>
+                            <div
+                                class="position-absolute w-100 h-100 top-0 d-flex flex-column justify-content-end px-5 dpb-60">
+                                <div class="bodoni font40 leading45 text-white dmb-10">
+                                    Vintage
+                                </div>
+                                <div class="mont-medium font14 leading24 text-white dmb-15">
+                                    The UK’s finest selection of domestic, bar and restaurant furniture
+                                </div>
+                                <div class="d-inline-flex">
+                                    <div class="mont-medium font13 leading24 text-white text-capitalize border-bottom">
+                                        Shop
+                                        Collection</div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-6 dmb-20 cards-col">
+                    <a href="" class="w-100 text-decoration-none">
+                        <div class="collection-cards w-100 radius10 position-relative overflow-hidden  card-hover">
+                            <img src="<?php echo get_home_url(); ?>/wp-content/uploads/2024/07/blog-1.png" alt=""
+                                class="w-100 h-100 object-cover">
+                            <div class="position-absolute w-100 h-100 top-0 bg-black opacity-25"></div>
+                            <div
+                                class="position-absolute w-100 h-100 top-0 d-flex flex-column justify-content-end px-5 dpb-60">
+                                <div class="bodoni font40 leading45 text-white dmb-10">
+                                    Storage
+                                </div>
+                                <div class="mont-medium font14 leading24 text-white dmb-15">
+                                    The UK’s finest selection of domestic, bar and restaurant furniture
+                                </div>
+                                <div class="d-inline-flex">
+                                    <div class="mont-medium font13 leading24 text-white text-capitalize border-bottom">
+                                        Shop
+                                        Collection</div>
+                                </div>
+                            </div>
                         </div>
                     </a>
                 </div>
@@ -101,7 +143,7 @@
         </div>
     </section>
 
-    <div class="spacing dmb-95"></div>
+    <div class="spacing dmb-130"></div>
 
     <!-- product-section -->
     <section class="product-section">
@@ -212,7 +254,7 @@
         </div>
     </section>
 
-    <div class="spacing dmb-80"></div>
+    <div class="spacing dmb-125"></div>
 
     <!-- room-card-section -->
     <section class="room-card-section bg-292929 dpt-80 dpb-70">
@@ -279,7 +321,7 @@
         <div class="position-absolute top-center start-0 w-100">
             <div class="container w-100">
                 <div class="col-8 ms-5 ps-5">
-                    <div class="bodoni font60 leading70 text-capitalize text-white dmb-10">Our Showroom</div>
+                    <div class="bodoni font60 leading70 text-capitalize text-white dmb-10">Seasonal Promotion</div>
                     <div class="mont-medium font14 leading20 text-white dmb-35">The UK’s finest selection of domestic,
                         bar and restaurant furniture</div>
                     <a href=""
@@ -291,74 +333,13 @@
         </div>
     </section>
 
-    <div class="dmb-95 "></div>
-
-    <!-- project-card-section -->
-    <section class="project-card-section">
-        <div class="container">
-            <div class="row dmb-55">
-                <div class="col-3"></div>
-                <div class="col-6 text-center">
-                    <div class="bodoni font50 leading55 text-black text-capitalize">Our Projects</div>
-                    <a href="" class="montserrat-medium font13 leading20 text-black dmt-10">Get in touch</a>
-                </div>
-                <div class="col-3">
-                    <div class="d-flex justify-content-end align-items-center h-100">
-                        <a href=""
-                            class="text-decoration-none btnA bg-292929-btn mont-semibold font11 leading20 space0_28 text-uppercase radius5 transition d-inline-flex align-items-center justify-content-center">
-                            VIEW ALL
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="row row10">
-                <div class="col-4 cards-col dmt-50">
-                    <a href=""
-                        class="project-cards text-decoration-none d-inline-block position-relative card-hover radius10 overflow-hidden w-100">
-                        <img src="<?php echo get_home_url(); ?>/wp-content/uploads/2024/07/project-1.png"
-                            class="w-100 h-100 object-cover img" alt="">
-                        <div class="position-absolute bottom-0 text-center w-100 px-5">
-                            <div class="bodoni font40 leading48 space1_65 text-white dpb-20">Whitemills Kitchen &
-                                Bar</div>
-                            <div class="mont-medium font11 leading16 text-white text-uppercase dpb-50">KENT</div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-4 cards-col dmt-50">
-                    <a href=""
-                        class="project-cards text-decoration-none d-inline-block position-relative card-hover radius10 overflow-hidden">
-                        <img src="<?php echo get_home_url(); ?>/wp-content/uploads/2024/07/project-2.png"
-                            class="w-100 h-100 object-cover img" alt="">
-                        <div class="position-absolute bottom-0 text-center w-100 px-5">
-                            <div class="bodoni font40 leading48 space1_65 text-white dpb-20">BrewDog</div>
-                            <div class="mont-medium font11 leading16 text-white text-uppercase dpb-50">LAS VEGAS
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-4 cards-col dmt-50">
-                    <a href=""
-                        class="project-cards text-decoration-none d-inline-block position-relative card-hover radius10 overflow-hidden">
-                        <img src="<?php echo get_home_url(); ?>/wp-content/uploads/2024/07/project-3.png"
-                            class="w-100 h-100 object-cover img" alt="">
-                        <div class="position-absolute bottom-0 text-center w-100 px-5">
-                            <div class="bodoni font40 leading48 space1_65 text-white dpb-20">Libertine Burger</div>
-                            <div class="mont-medium font11 leading16 text-white text-uppercase dpb-50">LEAMINGTON
-                                SPA</div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <div class="dmb-165"></div>
+    <div class="spacing dmb-85"></div>
 
     <!-- blog-card-section -->
     <section class="blog-card-section">
         <div class="container">
             <div class="d-flex justify-content-between align-items-center dmb-25">
-                <div class="bodoni font40 leading48 text-black text-capitalize">Latest News</div>
+                <div class="bodoni font40 leading48 text-black text-capitalize">Blog inspiration</div>
                 <a href=""
                     class="text-decoration-none btnA bg-292929-btn mont-semibold font11 leading20 space0_28 text-uppercase radius5 transition d-inline-flex align-items-center justify-content-center">
                     VIEW ALL
@@ -417,5 +398,5 @@
         </div>
     </section>
 
-    <div class="dmb-100"></div>
+    <div class="spacing dmb-140"></div>
 </main>
