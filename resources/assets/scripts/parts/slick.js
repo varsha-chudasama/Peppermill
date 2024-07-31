4e8cf8a5ffa6e3755c0216846393fb76c489ccee
varsha-chudasama <147918626+varsha-chudasama@users.noreply.github.com>
2024-07-31 15:03:28 +0530

    responsive footer done

--- resources/assets/scripts/parts/slick.js
+++ resources/assets/scripts/parts/slick.js
@@ -122,23 +122,18 @@ export class Slick {
                     breakpoint: 1024,
                     settings: {
                         slidesToShow: 3,
-                        slidesToScroll: 3,
-                        infinite: true,
-                        dots: true
                     }
                 },
                 {
                     breakpoint: 600,
                     settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2
+                        slidesToShow: 3,
                     }
                 },
                 {
                     breakpoint: 480,
                     settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
+                        slidesToShow: 3,
                     }
                 }
             ]
--- templates/footer.php
+++ templates/footer.php
@@ -1,46 +1,45 @@
-<!-- <footer class="footer">
-        <div class="top-footer dpt-40">
+<footer class="footer">
+        <div class="top-footer dpt-40 tpt-55">
             <div class="container">
-            <div class="text-center dpb-45">
-                <div class="bodoni font40 leading48 dmb-15 top-footer-text">#peppermillhomes</div>
+            <div class="text-center dpb-45 tpb-25">
+                <div class="bodoni font40 res-font30 leading48 res-leading35 dmb-15 tmb-20 top-footer-text">#peppermillhomes</div>
                 <div class="mont-normal font13 leading20 top-footer-text">Share your creation on Instagram using the
                     hashtag
                     #peppermillhomes</div>
             </div>
-            <div class="footer-slider dpb-50">
-                <div class="footer-img radius10 overflow-hidden position-relative card-hover">
+            <div class="footer-slider dpb-50 tpb-45">
+                <div class="footer-img radius10 res-radius5 overflow-hidden position-relative card-hover">
                     <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/three-card-3.png"
                         class="w-100 h-100 object-cover img" alt="">
                 </div>
-                <div class="footer-img radius10 overflow-hidden position-relative card-hover">
+                <div class="footer-img radius10 res-radius5 overflow-hidden position-relative card-hover">
                     <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/product-4.png"
                         class="w-100 h-100 object-cover img" alt="">
                 </div>
-                <div class="footer-img radius10 overflow-hidden position-relative card-hover">
+                <div class="footer-img radius10 res-radius5 overflow-hidden position-relative card-hover">
                     <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/three-card-2.png"
                         class="w-100 h-100 object-cover img" alt="">
                 </div>
-                <div class="footer-img radius10 overflow-hidden position-relative card-hover">
+                <div class="footer-img radius10 res-radius5 overflow-hidden position-relative card-hover">
                     <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/product-1.png"
                         class="w-100 h-100 object-cover img" alt="">
                 </div>
-                <div class="footer-img radius10 overflow-hidden position-relative card-hover">
+                <div class="footer-img radius10 res-radius5 overflow-hidden position-relative card-hover">
                     <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/footer-1.png"
                         class="w-100 h-100 object-cover img" alt="">
                 </div>
-                <div class="footer-img radius10 overflow-hidden position-relative card-hover">
+                <div class="footer-img radius10 res-radius5 overflow-hidden position-relative card-hover">
                     <img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/product-4.png"
                         class="w-100 h-100 object-cover img" alt="">
                 </div>
             </div>
         </div>
-    </div>
-    <div class="container">
-        <div class="bottom-footer dpt-80 dpb-45">
-            <div class="row dpb-130">
-                <div class="col-2">
-                    <ul class="list-none footer-content mb-0 px-0">
-                        <li class="dmb-30 font26 leading30 text-white">
+    <div class="container px-p-0">
+        <div class="bottom-footer dpt-80 tpt-0 dpb-45 tpb-0">
+            <div class="row flex-column flex-lg-row dpb-130 tpb-40">
+                <div class="col-lg-2 col-12 top-border tpt-30 tpb-30">
+                    <ul class="list-none footer-content mb-0 px-0 px-p-p">
+                        <li class="dmb-30 tmb-20 font26 res-font22 leading30 text-white">
                             How we can help </li>
                         <li class="dmb-5"><a href=""
                                 class="text-decoration-none mont-normal font13 leading20 text-white ">Delivery
@@ -66,9 +65,9 @@
                         </li>
                     </ul>
                 </div>
-                <div class="col-2">
-                    <ul class="list-none footer-content mb-0 px-0">
-                        <li class="dmb-30 font26 leading30 text-white">
+                <div class="col-lg-2 col-12 top-border tpt-30 tpb-30">
+                    <ul class="list-none footer-content mb-0 px-0 px-p-p">
+                        <li class="dmb-30 tmb-20 font26 res-font22 leading30 text-white">
                             About us </li>
                         <li class="dmb-5"><a href=""
                                 class="text-decoration-none mont-normal font13 leading20 text-white ">About
@@ -91,9 +90,9 @@
                         </li>
                     </ul>
                 </div>
-                <div class="col-2">
-                    <ul class="list-none footer-content mb-0 px-0">
-                        <li class="dmb-30 font26 leading30 text-white">
+                <div class="col-lg-2 col-12 top-border tpt-30 tpb-30">
+                    <ul class="list-none footer-content mb-0 px-0 px-p-p">
+                        <li class="dmb-30 tmb-20 font26 res-font22 leading30 text-white">
                             Inspiration</li>
                         <li class="dmb-5"><a href=""
                                 class="text-decoration-none mont-normal font13 leading20 text-white ">Blog</a>
@@ -116,13 +115,13 @@
                         </li>
                     </ul>
                 </div>
-                <div class="col-6">
-                    <div class="col-8 ms-auto">
-                        <div class="bodoni font26 leading30 text-white dmb-30">Sign up to our newsletter</div>
-                        <div class="mont-medium font13 leading20 text-white dmb-20">Don’t miss out on our latest
+                <div class="col-lg-6 col-12 tpt-40 top-border">
+                    <div class="col-lg-8 col-12 ms-lg-auto px-p-p">
+                        <div class="bodoni font26 res-font22 leading30 text-white dmb-30 tmb-25">Sign up to our newsletter</div>
+                        <div class="mont-medium font13 leading20 text-white dmb-20 tmb-35">Don’t miss out on our latest
                             arrivals,
                             promotions and discounts:</div>
-                        <div class="d-flex input-row dmb-15">
+                        <div class="d-flex input-row dmb-15 tmb-20">
                             <div class="pe-2 w-100">
                                 <input type="text"
                                     class="input-field w-100 mont-medium font12 leading16 text-918E8E radius5 px-4"
@@ -135,7 +134,7 @@
                                 </a>
                             </div>
                         </div>
-                        <div class="mont-medium font11 leading16 text-white dmb-15">What would you like to receive?
+                        <div class="mont-medium font11 leading16 text-white dmb-15 tmb-30">What would you like to receive?
                         </div>
                         <div class="radio-btns d-flex">
                             <div class="radio-btn">
@@ -163,47 +162,47 @@
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-4">
+            <div class="row flex-column flex-lg-row top-border tpt-40">
+                <div class="col-lg-4 col-12 order-3 order-lg-1 tmb-50 px-p-p">
                     <ul class="d-flex list-none footer-ul mb-0 px-0">
-                        <li class="me-4"><a href="" class="d-inline-block footer-icon"><img
+                        <li class="me-lg-4 me-1"><a href="" class="d-inline-block footer-icon"><img
                                     src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/footer-icon-1.png"
                                     class="w-100" alt=""></a></li>
-                        <li class="me-4"><a href="" class="d-inline-block footer-icon"><img
+                        <li class="me-lg-4 me-1"><a href="" class="d-inline-block footer-icon"><img
                                     src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/footer-icon-2.png"
                                     class="w-100" alt=""></a></li>
-                        <li class="me-4"><a href="" class="d-inline-block footer-icon"><img
+                        <li class="me-lg-4 me-1"><a href="" class="d-inline-block footer-icon"><img
                                     src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/footer-icon-3.png"
                                     class="w-100" alt=""></a></li>
-                        <li class="me-4"><a href="" class="d-inline-block footer-icon"><img
+                        <li class="me-lg-4 me-1"><a href="" class="d-inline-block footer-icon"><img
                                     src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/footer-icon-4.png"
                                     class="w-100" alt=""></a></li>
-                        <li class="me-4"><a href="" class="d-inline-block footer-icon"><img
+                        <li class="me-lg-4 me-1"><a href="" class="d-inline-block footer-icon"><img
                                     src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/footer-icon-5.png"
                                     class="w-100" alt=""></a></li>
-                        <li class="me-4"><a href="" class="d-inline-block footer-icon"><img
+                        <li class="me-lg-4 me-1"><a href="" class="d-inline-block footer-icon"><img
                                     src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/footer-icon-6.png"
                                     class="w-100" alt=""></a></li>
                     </ul>
                 </div>
-                <div class="col-5">
-                    <ul class="list-none footer-ul mb-0 px-0 d-flex align-items-center h-100">
-                        <li class="me-4"><a href=""
+                <div class="col-lg-5 col-12 order-2 tmb-50 px-p-p">
+                    <ul class="list-none footer-ul mb-0 px-0 d-flex flex-wrap align-items-center h-100">
+                        <li class="col-6 col-lg-3"><a href=""
                                 class="text-decoration-none mont-semibold font9 leading11 space0_23 text-uppercase text-white">TERMS
                                 & CONDITIONS</a></li>
-                        <li class="me-4"><a href=""
+                        <li class="col-6 col-lg-3"><a href=""
                                 class="text-decoration-none mont-semibold font9 leading11 space0_23 text-uppercase text-white">PRIVACY
                                 POLICY</a></li>
-                        <li class="me-4"><a href=""
+                        <li class="col-6 col-lg-3"><a href=""
                                 class="text-decoration-none mont-semibold font9 leading11 space0_23 text-uppercase text-white">PAYMENT
                                 METHODS</a></li>
-                        <li class="me-4"><a href=""
+                        <li class="col-6 col-lg-3"><a href=""
                                 class="text-decoration-none mont-semibold font9 leading11 space0_23 text-uppercase text-white">DESIGN
                                 BY THE CURIOUS</a></li>
                     </ul>
                 </div>
-                <div class="col-3">
-                    <ul class="d-flex footer-ul list-none mb-0 px-0 justify-content-end">
+                <div class="col-lg-3 col-12 order-1 order-lg-3 tmb-50 px-p-p">
+                    <ul class="d-flex footer-ul list-none mb-0 px-0 justify-content-lg-end">
                         <li class="me-3"><a
                                 class="footer-icon-bg rounded-pill bg-white d-inline-flex justify-content-center align-items-center"
                                 href=""><img src="<?php echo get_home_url() ?>/wp-content/uploads/2024/07/facebook.svg"
@@ -225,7 +224,7 @@
             </div>
         </div>
     </div>
-</footer> -->
+</footer>
 
 <script>
     function myFunction() {
